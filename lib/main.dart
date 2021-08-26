e9fce45b42fd0cc1d9978d57a650bcf5324bce1d
darknet <nkrezeski1@gmail.com>
2021-08-26 02:19:21 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(GoldHosting());
+}
+
+class GoldHosting extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gold-Hosting Client',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gold-Hosting Client'),
+        backgroundColor: Color(0xFF202c3c),
+      ),
+      body: WebView(
+        initialUrl: 'https://gold-hosting.me/panel/',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
